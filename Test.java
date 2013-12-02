b3747d75ce2c8b552423a86e486eef6c4b6274c8
txwh <txwh1029@gmail.com>
2013-12-02 15:08:07 +0800

    aaaaa

--- Test.java
+++ Test.java
@@ -1,9 +1,11 @@
 public class Test
 {
 	public static void main(String args[]){
-	
+		int a=10;
+		System.out.println("a="+a);
 		System.out.println("hello");
-		System.out.println("����,����");
+		System.out.println("????,????");
 		System.out.println("zhansang");
+
 	}
 }
