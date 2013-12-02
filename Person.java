447d33847c7daafb7d67c3a35acf5a132fd51383
txwh <txwh1029@gmail.com>
2013-12-02 15:40:57 +0800

    the second commit person

---
+++ Person.java
@@ -0,0 +1,18 @@
+public class Person
+{	
+	String name;
+	int age;
+	public void setName(String name){
+		this.name=name;
+	}
+	public String getName(){
+		return this.name;
+	}
+
+	public void setAge(int age){
+		this.age=age;
+	}
+	public int getAge(){
+		return this.age;
+	}
+}
